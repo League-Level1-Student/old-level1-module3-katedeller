95e572e9d68d99cb49378e16865fcc69a1d9a39f
Rohan890 <nootnoot@gmail.com>
2018-10-07 16:41:29 -0700

    nastysurprise

---
+++ src/NastySurprise.java
@@ -0,0 +1,25 @@
+import java.awt.Component;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class NastySurprise {
+public static void main(String[] args) {
+	NastySurprise nasty = new NastySurprise();
+}
+	public NastySurprise() {
+	
+		// TODO Auto-generated constructor stub
+		JFrame frame = new JFrame();
+		frame.setVisible(true);
+		JPanel panel = new JPanel();
+	frame.add(panel);
+JButton trick = new JButton();
+		JButton treat = new JButton();
+		panel.add(trick);
+		panel.add(treat);
+		frame.pack();
+	}
+
+}
