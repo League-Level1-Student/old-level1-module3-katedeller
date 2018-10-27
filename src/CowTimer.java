7fb1a71af16315799837afae91881a9fdeea8829
League <League@adminisorsimac2.attlocal.net>
2018-10-27 11:21:16 -0700

    Book of illusions, cow timer, 1/2 of jeopardy

--- src/CowTimer.java
+++ src/CowTimer.java
@@ -6,38 +6,51 @@
 import java.applet.AudioClip;
 import java.io.IOException;
 import javax.swing.JApplet;
+import javax.swing.JOptionPane;
 
 public class CowTimer {
 	/*
 	 * This is an advanced recipe. There may be more than one line of code for each
 	 * instruction. Work in seconds when testing, then change to minutes
 	 */
-
+	static CowTimer one = new CowTimer();
+	int i = 0;
 	public static void main(String[] args) throws InterruptedException {
 		/* 1. Make a CowTimer, set the time and start it. */
 
+one.setTime(3);
+one.start();
 	}
 
-	private int minutes;
+	private int seconds;
 
-	public void setTime(int minutes) {
-		this.minutes = minutes;
-		System.out.println("Cow set to " + minutes + " minutes.");
+	public void setTime(int seconds) {
+		this.seconds = seconds;
+		System.out.println("Cow set to " + seconds + " seconds.");
 	}
 
 	public void start() throws InterruptedException {
 		/*
-		 * 2. Count down the minutes, print the current minute then sleep for 60 seconds
+		 * 2. Count down the minutes, print the current second then sleep for 60 seconds
 		 * using Thread.sleep(int milliseconds).
 		 */
-
+for (i = 3; i > 0; i--) {
+	
+	Thread.sleep(1000);
+	JOptionPane.showMessageDialog(null, i);
+}
 		/*
 		 * 3. When the timer is finished, use the playSound method to play a moo sound.
+
 		 * You can use the .wav file in the default package, or you can download one
 		 * from freesound.org, then drag it intothe default package.
 		 */
 
+
+	playSound("moo.wav");
 	}
+
+	
 
 	private void playSound(String fileName) {
 		AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName));
--- src/Jeopardy.java
+++ src/Jeopardy.java
@@ -44,19 +44,20 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
-		// 2. Give your frame a title
-
+frame.setVisible(true);
+		
+// 2. Give your frame a title
+frame.setName("B");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+JPanel j1 = createHeader("A");
 		// 4. Add the header component to the quizPanel
-
+quizPanel.add(j1);
 		// 5. Add the quizPanel to the frame
-
+quizPanel.add(frame);
 		// 6. Use the createButton method to set the value of firstButton
-
-		// 7. Add the firstButton to the quizPanel
-
+JButton b1 = createButton("$200")
+;		// 7. Add the firstButton to the quizPanel
+quizPanel.add(b1);
 		// 8. Write the code to complete the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
 
@@ -87,11 +88,11 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-
+JButton button = new JButton();
 		// Set the text of the button to the dollarAmount
-
+button.setText(dollarAmount);
 		// Increment the buttonCount (this should make the layout vertical)
-
+buttonCount ++;
 		// Return your new button instead of the temporary button
 
 		return new JButton("temporary button");
