8405ae2784e499d436782cad22e711ca4336ab2a
league <league@wts-imac-11.attlocal.net>
2018-12-02 17:18:11 -0800

    calculator

---
+++ src/calculator.java
@@ -0,0 +1,21 @@
+import javax.swing.JOptionPane;
+
+public class calculator {
+public static void main(String[] args) {
+	calculator calculator = new calculator();
+	calculator.add(1, 2);
+	calculator.subtract(17, 5);
+	calculator.multiply(9, 32);
+}
+void add(int a, int b) {
+int answer= a+b;
+JOptionPane.showMessageDialog(null, answer);
+}
+void subtract(int a, int b) {
+	int answer=a-b;
+	JOptionPane.showMessageDialog(null, answer);
+}
+void multiply(int a, int b) {
+	int answer=ab;
+}
+}
